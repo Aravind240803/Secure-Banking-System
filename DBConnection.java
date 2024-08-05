e0c7630ef48ebbb5ac0b74eaec76c429596c45ac
Aravind240803 <115934858+Aravind240803@users.noreply.github.com>
2024-08-05 13:06:18 +0530

    DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/secure_banking";
     private static final String USER = "root";
-    private static final String PASSWORD = "Aravind@3824";
+    private static final String PASSWORD = "password";
 
     static {
         try {
